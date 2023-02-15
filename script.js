0d2ac797648c5519cf904d3a84bbfd040365817f
Victoria Vita <victoria.vita94@gmail.com>
2023-02-15 16:10:40 -0300

    array, objetos, funciones orden

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ let userName = prompt(
 alert(`Hola ${userName} a continuacion le ofreceremos distintas opciones`);
 
 // simulador de opciones
+ // funcion constructora objeto
 
+class Opcion{
+    constructor (cantidad, arancel){
+        this.cantidad= cantidad;
+        this.arancel= arancel;
+    }
+}
+const opcionUno = new Opcion ("dos", 4600)
+const opcionDos = new Opcion ("tres", 5100)
+const opcionTres = new Opcion ("free pass", 6400)
 
+const arrayOpcion = [opcionUno, opcionDos, opcionTres];
+console.log(arrayOpcion);
 
 let planGim = prompt("Seleccione una opción: \n opcion a: 2 veces por semana \n opcion b: 3 veces por semana \n opcion c: free pass \n opcion d: no me interesa ninguna opcion")
+
 while (planGim != "opcion a" && planGim != "opcion b" && planGim != "opcion c" && planGim != "opcion d"){ 
     alert("Por favor ingrese una opción correcta")
     planGim = prompt("Seleccione una opción: \n opcion a: 2 veces por semana \n opcion b: 3 veces por semana \n opcion c: free pass \n opcion d: no me interesa ninguna opcion")
@@ -23,6 +36,7 @@ while (planGim != "opcion a" && planGim != "opcion b" && planGim != "opcion c" &
     alert(`${userName} Gracias por ingresar a The Box.`)
 } 
 
+
 // agregar remera para entrenar
 
 
@@ -31,7 +45,6 @@ while (agregarProducto != "si" && agregarProducto != "no"){
     alert("por favor ingrese una opcion correcta")
     agregarProducto = prompt (`${userName} ¿Desea agregar a su opcion elegida una remera para entrenar Dry fit? Ingrese si o no`)
 }if (agregarProducto == "si"){
-
     let remera = 2000
     function sumar (opcion,remera) {
         console.log ("El valor de su arancel final es de" + (parseFloat(opcion) + parseFloat(remera)))
@@ -42,3 +55,34 @@ while (agregarProducto != "si" && agregarProducto != "no"){
 }else if (agregarProducto == "no"){
     alert("Gracias por visitarnos.")
 }
+
+agregarProducto = {
+    producto: "remera",
+    color: "negro",
+    precio: 2000
+};
+console.log(agregarProducto);
+
+
+
+
+arrayOpcion.forEach(opcion=>console.log(opcion.arancel));
+
+const arrayOpcionRemera = arrayOpcion.map((opcion)=>{
+    return{
+        cantidad: opcion.cantidad,
+        arancel: (opcion.arancel + 2000)
+    }
+});
+console.log(arrayOpcionRemera);
+
+
+
+const totalArancel = arrayOpcion.reduce((acc, e)=> acc + e.arancel, 0);
+console.log(`El total es de: ${totalArancel}`)
+
+
+
+
+
+
